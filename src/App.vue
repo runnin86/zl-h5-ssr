81924345f79b96aad27f17b34012ecc528ecee04
runnin86 <dda1314@163.com>
2018-10-31 17:16:55 +0800

    2018-10-31 17:16:26
    抽离底部菜单组件

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,188 @@
 <template>
-<div id="app">
-  <Head></Head>
-  <transition name="fade" mode="out-in">
-    <main id="main">
-      <router-view class="view"></router-view>
-    </main>
-  </transition>
-</div>
+  <div id="app" class="page__bd">
+    <Head></Head>
+    <transition :name="$router.options.transitionName" mode="out-in">
+      <keep-alive>
+        <router-view class="container container-content viewStyle"></router-view>
+      </keep-alive>
+    </transition>
+
+    <wv-tabbar :isShow="true" v-bind:itemList="tabbarList" fixed></wv-tabbar>
+
+    <!-- 悬浮导航 -->
+    <div class="quickIcon" v-if = "$route.path!=='/shopGoods/goodsInfo' && $route.path!=='/shopping/goods'">
+      <img v-if="showQuickIcon!=='show'" src="/static/images/smallIcon/entry.png" alt="" @click="showQuickIcon='show'">
+      <div :class="showQuickIcon === 'default' ? 'quickCont' : (showQuickIcon === 'show' ? 'quickCont quickContAni' : 'quickCont quickHideAni')">
+        <!-- back -->
+        <img class="quickBack" @click="back" src="/static/images/smallIcon/back.png" alt="">
+        <!-- classify -->
+        <!-- <router-link :to="{path: '/classify'}" v-if = "$route.path!=='/classify'">
+          <img class="quickCat" src="/static/images/smallIcon/category.png" alt="">
+        </router-link>
+        <img class="quickCat" v-else @click="showQuickIcon='hide'" src="/static/images/smallIcon/category.png" alt=""> -->
+        <!-- home -->
+        <router-link :to="{path: '/index'}" v-if = "$route.path!=='/index'">
+          <img class="quickHome" src="/static/images/smallIcon/home.png" alt="">
+        </router-link>
+        <img v-else @click="showQuickIcon='hide'" class="quickHome" src="/static/images/smallIcon/home.png" alt="">
+        <!-- close -->
+        <img class="quickClose" src="/static/images/smallIcon/close.png" alt="" @click="showQuickIcon='hide'">
+        <!-- search -->
+        <!-- <router-link :to="{path: '/userCenter/myCollect'}" v-if = "$route.path!=='/userCenter/myCollect'">
+          <img class="quickSearch" src="/static/images/smallIcon/collect.png" alt="">
+        </router-link>
+        <img class="quickSearch" v-else @click="showQuickIcon='hide'" src="/static/images/smallIcon/collect.png" alt=""> -->
+        <!-- share or collect -->
+        <!-- <img v-if ="$route.path==='/index'" class="quickQrcode" src="/static/images/smallIcon/share.png" alt="" @click="freshQRCodeShow">
+        <router-link v-else-if="$route.path!=='/searchGoods'" :to="{path: '/searchGoods'}">
+          <img class="quickQrcode" src="/static/images/smallIcon/search_index.png" alt="">
+        </router-link>
+        <img v-else @click="showQuickIcon='hide'" class="quickQrcode" src="/static/images/smallIcon/search_index.png" alt=""> -->
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import Head from './components/head.vue'
+import {mapGetters} from 'vuex'
+import thumbSmall from 'static/weui/images/icon_tabbar.png'
+
+const menuList = [
+  {title: '商城', path: '/index', icon: '/static/images/sale_c.png', activeIcon: '/static/images/sale.png', activePath: ['/index', '/category']},
+  {title: '购物车', path: '/cart', icon: '/static/images/shop_c.png', activeIcon: '/static/images/shop.png', cartBadgeNum: 0},
+  {title: '我的', path: '/userCenter', icon: '/static/images/user_c.png', activeIcon: '/static/images/user.png'}
+]
 
 export default {
   name: 'app',
   // 数据
   data() {
-    return {}
+    return {
+      thumbSmall,
+      toPath: null,
+      fromPath: null,
+      showQuickIcon: 'default',  // 快捷按钮是否打开
+      imgBase64: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQIW2NkAAIAAAoAAggA9GkAAAAASUVORK5CYII=',
+      tabbarList: menuList
+    }
   },
   // 计算属性
   computed: {
-
+    ...mapGetters({
+      // 键为在当前页面展示,值为modules中getters中定义的键
+      products: 'cartProducts',
+      checkoutStatus: 'checkoutStatus',
+      cartBadgeNum: 'cartBadge',
+      showIndex: 'isIndex'
+    }),
+    total () {
+      return this.products.reduce((total, p) => {
+        return total + p.price * p.quantity
+      }, 0)
+    }
   },
   mounted() {
-
+    let self = this
+    setTimeout(function() {
+      self.showQuickIcon = 'show'
+      setTimeout(function() {
+        self.showQuickIcon = 'hide'
+      }, 500)
+    }, 1000)
   },
   // 方法
   methods: {
-    test() {
-
+    /**
+     * 判断微信浏览器
+     */
+    is_weixn() {
+      let ua = window.navigator.userAgent.toLowerCase()
+      if (ua.indexOf('micromessenger') !== -1) {
+        return true
+      } else {
+        return false
+      }
+    },
+    gotoAddress (path, query) {
+      this.$router.push({path: path, query: query})
+    },
+    back () {
+      this.$router.back()
+      // window.history.back()
+      // window.history.go(-1)
+    },
+    randomString (len) {
+      // 默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
+      let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+      let maxPos = $chars.length
+      let pwd = ''
+      for (let i = 0; i < len; i++) {
+        pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+      }
+      return pwd
+    },
+    getShareImg () {
+      // 分享图片地址
+      let imgUrl = window.location.origin + '/static/images/logo-new1.jpeg'
+      if (window.localStorage.getItem('zlUser') !== null) {
+        // 默认展示用户头像
+        imgUrl = JSON.parse(window.localStorage.getItem('zlUser')).headImgUrl
+      }
+      return imgUrl
+    },
+    setTitle(t) {
+      document.title = t
+      var i = document.createElement('iframe')
+      // i.src = '//m.baidu.com/favicon.ico'
+      i.style.display = 'none'
+      i.onload = function() {
+        setTimeout(function() {
+          i.remove()
+        }, 9)
+      }
+      document.body.appendChild(i)
     }
   },
   // 子组件
   components: {
     Head
+  },
+  // dynamically set transition based on route change
+  watch: {
+    '$route' (to, from) {
+      this.toPath = to.path
+      this.fromPath = from.path
+      // 页面需要滚动到顶部
+      if (to.matched.some(m => m.meta.scrollToTop)) {
+        setTimeout(_ => {
+          document.body.scrollTop = 0
+        }, 300)
+      }
+      this.showQuickIcon = 'default'
+      // 默认全局分享
+      let ignoreUrl = [
+        '/index',
+        '/index/sevenPanicBuy',
+        '/shopping/goods',
+        '/userCenter/shareQRCode',
+        '/oauth',
+        '/favicon.ico'
+      ]
+      if (!ignoreUrl.includes(to.path)) {
+        let base = this.$router.options.base ? this.$router.options.base : ''
+        // 分享图片地址
+        let imgUrl = this.getShareImg()
+        // 默认全局分享
+        let desc = '【足力购】帅哥美女们，快来足力购逛逛，捧个场吧！'
+        let link = window.location.origin + base + '/?#/category'
+        this.initWechatShare(
+          '足力购商城欢迎您',
+          desc,
+          imgUrl,
+          link
+        )
+      }
+    }
   }
 }
 </script>
--- src/components/tabbar/tabbar.vue
+++ src/components/tabbar/tabbar.vue
@@ -1,6 +1,15 @@
 <template>
-	<div class="weui-tabbar container container-content" :style="{ position: fixed ? 'fixed' : 'absolute' }">
-		<slot></slot>
+	<div v-if="isShow" class="weui-tabbar container container-content tabbar1" :style="{position: fixed ? 'fixed' : 'absolute'}">
+		<wv-tabbar-item v-for="item in itemList" :key="item.path"
+		  :to="item.path" :is-on="item.activePath ? item.activePath.includes(item.path) : $route.path===item.path">
+      <span slot="icon" class="botMenu">
+				<!-- 菜单图标 -->
+        <img :src="$route.path===item.path?item.activeIcon:item.icon"/>
+				<!-- 购物图标 -->
+				<wv-badge v-show="item.cartBadgeNum" class="badge">{{item.cartBadgeNum}}</wv-badge>
+      </span>
+      {{item.title}}
+    </wv-tabbar-item>
 	</div>
 </template>
 
@@ -9,7 +18,22 @@ export default {
   name: 'wv-tabbar',
 
   props: {
-    fixed: Boolean
+    fixed: Boolean,
+    isShow: Boolean,
+    itemList: Array
   }
 }
 </script>
+
+<style>
+.tabbar1 {
+	table-layout: fixed;
+	box-shadow:0 -5px 30px rgba(0,0,0,0.1);
+}
+.badge {
+	position: absolute;
+	top: 0;
+	right: -10px;
+	padding:.10em .43em;
+}
+</style>
